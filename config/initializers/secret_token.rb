ec5efee8fc7c544844f1fa6042d33c50e4e76661
Yu Jiang Tham <ytham@apple.com>
2013-03-04 19:43:45 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QlDatabase::Application.config.secret_token = '4ed14c7c1a5bc1bcaf35a3a1e71b9c4577c4153dd92ec728dbe6017169eac4edb3e95ad98b9bf57f528a8e6da13edba6a6c51486b39ddf5190cca435affae07f'
